e7098c2f5f81e86b795197a02096fea261d6f8d9
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-16 18:21:54 +0630

    create language table

--- database/migrations/2024_09_16_111853_create_languages_table.php
+++ database/migrations/2024_09_16_111853_create_languages_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
+            $table->string('lang_code');
+            $table->string('lang_name');
+            $table->string('is_default')->default('No');
+            $table->integer('status')->default(0);
+            $table->string('lang_direction');
             $table->timestamps();
         });
     }
