ba631ad5c31fc72b4450609506c99ec95fac809f
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-17 14:04:18 +0630

    create footer social links table

--- database/migrations/2024_09_16_112323_create_footer_social_links_table.php
+++ database/migrations/2024_09_16_112323_create_footer_social_links_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('footer_social_links', function (Blueprint $table) {
             $table->id();
+            $table->string('link')->nullable();
+            $table->string('icon')->nullable();
             $table->timestamps();
         });
     }
