5b26da6ef3ba21d6e9e9b1f20de6f0cedf7228ff
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-24 13:40:30 +0630

    create home page language table

---
+++ app/Models/HomepageLanguage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomepageLanguage extends Model
+{
+    use HasFactory;
+}
