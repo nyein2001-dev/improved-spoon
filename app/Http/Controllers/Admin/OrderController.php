ddd5ac21c9e11a076228616fecea526318f05fb8
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-10-04 15:18:14 +0630

    create order views

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index(Request $request)
+    {
+        $title = trans('admin_validation.All Order');
+        return view('admin.order', compact('title'));
+    }
 }
---
+++ resources/views/admin/order.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.master_layout')
+@section('title')
+    <title>{{ $title }}</title>
+@endsection
+@section('admin-content')
+    <!-- Main Content -->
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>{{ $title }}</h1>
+            </div>
+        </section>
+    </div>
+@endsection
